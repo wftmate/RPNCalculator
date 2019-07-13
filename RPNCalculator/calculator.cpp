9ee13a48684fef9ca5bc405d7996f626ea8eae80
wftmate <williamfitchturner@gmail.com>
2019-07-13 14:36:26 -0400

    Inital gui setup complete

---
+++ RPNCalculator/calculator.cpp
@@ -0,0 +1,6 @@
+#include "calculator.h"
+
+calculator::calculator()
+{
+
+}
---
+++ RPNCalculator/calculator.h
@@ -0,0 +1,12 @@
+#ifndef CALCULATOR_H
+#define CALCULATOR_H
+
+#include <QWidget>
+
+class calculator
+{
+public:
+    calculator();
+};
+
+#endif // CALCULATOR_H
